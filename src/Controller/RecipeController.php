0fb70ba45c5c60a1cf41c90380877def545c62c2
Quartz-42 <bbaroche42@gmail.com>
2025-10-08 22:10:09 +0200

    Add Home and Recipe Controllers + views

---
+++ src/Controller/RecipeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class RecipeController extends AbstractController
+{
+    #[Route('/recettes', name: 'recipe.index')]
+    public function index(): Response
+    {
+        return $this->render('recipe/index.html.twig');
+    }
+
+    #[Route('/recettes/{slug}-{id}', name: 'recipe.show', requirements: ['slug' => '[a-z0-9\-]*', 'id' => '\d+'])]
+    public function show(string $slug, int $id): Response
+    {
+        return $this->render('recipe/show.html.twig', [
+            'slug' => $slug,
+            'id' => $id,
+        ]);
+    }
+}
