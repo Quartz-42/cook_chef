1baeceeba0b34329680b63dab586cacf63f64eaf
Quartz-42 <bbaroche42@gmail.com>
2025-10-08 23:09:52 +0200

    Add recipe management functionality with total duration display and update models and views

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -5,21 +5,33 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\RecipeRepository;
 
 final class RecipeController extends AbstractController
 {
     #[Route('/recettes', name: 'recipe.index')]
-    public function index(): Response
+    public function index(RecipeRepository $recipeRepository): Response
     {
-        return $this->render('recipe/index.html.twig');
+        $recipes = $recipeRepository->findAll();
+        $totalDuration = $recipeRepository->findTotalDuration();
+
+        return $this->render('recipe/index.html.twig', [
+            'recipes' => $recipes,
+            'totalDuration' => $totalDuration,
+        ]);
     }
 
     #[Route('/recettes/{slug}-{id}', name: 'recipe.show', requirements: ['slug' => '[a-z0-9\-]*', 'id' => '\d+'])]
-    public function show(string $slug, int $id): Response
+    public function show(string $slug, int $id, RecipeRepository $recipeRepository): Response
     {
+        $recipe = $recipeRepository->find($id);
+
+        if ($recipe->getSlug() !== $slug) {
+            return $this->redirectToRoute('recipe.show', ['slug' => $recipe->getSlug(), 'id' => $recipe->getId()]);
+        }
+
         return $this->render('recipe/show.html.twig', [
-            'slug' => $slug,
-            'id' => $id,
+            'recipe' => $recipe,
         ]);
     }
 }
---
+++ src/Repository/RecipeRepository.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Recipe;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Recipe>
+ */
+class RecipeRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Recipe::class);
+    }
+
+    public function findTotalDuration(): int
+    {
+        return (int) $this->createQueryBuilder('r')
+            ->select('SUM(r.duration) as totalDuration')
+            ->getQuery()
+            ->getSingleScalarResult();
+    }
+}
