48a459e539a3dd3240ef898f06fa6d127995ad92
Quartz-42 <bbaroche42@gmail.com>
2025-10-13 16:16:58 +0200

    feat(ux): add Symfony UX Toggle Password and Icons bundles, update registration form and templates

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -31,6 +31,7 @@ class RegistrationFormType extends AbstractType
                 // this is read and encoded in the controller
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
+                'toggle'=> true,
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
